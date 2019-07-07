795cf76f3012f2bd6fac90b23b0877142f5e7c21
cindyfeldman <Guitar2015!>
2019-07-07 11:29:24 -0700

    custom array list

--- src/_01_Custom_ArrayList/ArrayList.java
+++ src/_01_Custom_ArrayList/ArrayList.java
@@ -3,25 +3,46 @@ package _01_Custom_ArrayList;
 @SuppressWarnings("unchecked")
 
 public class ArrayList <T>{
-	
+	T[] h ;
 	public ArrayList() {
+		 h = (T[])new Object[0];
 	}
+	
 	
 	public T get(int loc) throws IndexOutOfBoundsException {
+		if(loc>=h.length&&loc<=h.length) {
+	throw new IndexOutOfBoundsException() ;
+		}
 		
-		return null;
+		return h[loc];
 	}
-	
 	public void add(T val) {
-		
+	T[] n = (T[]) new Object[h.length+1];
+	for (int i = 0; i < h.length; i++) {
+		n[i] = h[i];
+	}
+	n[n.length-1] = val;
+	h = n;
 	}
 	
 	public void insert(int loc, T val) throws IndexOutOfBoundsException {
-		
+		if(loc>=h.length&&loc<=h.length) {
+	throw new IndexOutOfBoundsException() ;
+		}
+
+	else {
+		h[loc] = val;
+	}
 	}
 	
 	public void set(int loc, T val) throws IndexOutOfBoundsException {
-		
+		if(loc>=h.length&&loc<=h.length) {
+	throw new IndexOutOfBoundsException() ;
+		}
+	
+	else {
+		h[loc] = val;
+	}
 	}
 	
 	public void remove(int loc) throws IndexOutOfBoundsException {
@@ -32,4 +53,11 @@ public class ArrayList <T>{
 		
 		return false;
 	}
+
+	public int size() {
+
+		return h.length;
+	}
+
+	
 }
